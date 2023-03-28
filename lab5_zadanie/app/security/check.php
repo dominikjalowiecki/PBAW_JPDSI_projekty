ecd708bbb1495235726e506725b0f9f735bf0798
Dominik Jałowiecki <dominik.jalowiecki1@gmail.com>
2023-03-28 20:45:38 +0200

    Fixed authorization sentinel to redirect directly to login action

--- lab5_zadanie/app/security/check.php
+++ lab5_zadanie/app/security/check.php
@@ -4,6 +4,6 @@ session_start();
 $role = $_SESSION['role'] ?? null;
 
 if ($role === null) {
-    include $config->root_path . '/app/security/login/login.php';
+    header('Location: ' . $config->action_url . 'login');
     exit();
 }
--- lab5_zadanie/app/security/login/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once $config->root_path . '/app/security/login/LoginController.class.php';
-
-
-$controller = new LoginController();
-$controller->process();
